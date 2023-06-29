0641dd4bcf909cbadeeb1a9d61b290f9aa9966d6
“guilherme” <lumertz2001@gmail.com>
2023-06-28 21:05:46 -0300

    visual chances - buttons, profile, text scroll

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ function toggleMode() {
 
   html.classList.toggle("light")
 }
+
+let text = document.getElementById("copy-text").innerHTML
+const copyContent = async () => {
+  try {
+    await navigator.clipboard.writeText(text)
+    console.log("Content copied to clipboard")
+  } catch (err) {
+    console.error("Failed to copy: ", err)
+  }
+}
